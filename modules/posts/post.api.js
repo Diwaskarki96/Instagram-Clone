b9af4e09f4a25a5a08dfb27829c7f4c99258212e
Diwash Karki <diwaskarki96@gmail.com>
2024-03-02 23:48:43 +0545

    added multer

--- modules/posts/post.api.js
+++ modules/posts/post.api.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userModel = require("../users/user.model");
 const postModel = require("./post.model");
+const upload = require("../../utils/multer");
+const { isAuthenticated } = require("../../utils/passport");
 
 router.get("/", async (req, res) => {
   const createdPost = await postModel.create({
@@ -13,6 +15,30 @@ router.get("/", async (req, res) => {
   res.json({ data: createdPost, msg: "success" });
 });
 
+//-------------upload---------------
+router.post(
+  "/upload",
+  upload.single("file"),
+  isAuthenticated,
+  async (req, res) => {
+    if (!req.file) {
+      return res.json({ data: "no files were uploaded" });
+    }
+    const user = await userModel.findOne({ email: req.user.email });
+
+    const post = await postModel.create({
+      image: req.file.filename,
+      postText: req.body.caption,
+      user: user._id,
+    });
+    user.posts.push(post._id);
+    await user.save();
+    res.send("done");
+    // res.redirect("/api/v1/user/profile");
+  }
+);
+
+//----------------------------------------
 router.get("/allposts", async (req, res) => {
   const allPosts = await userModel
     .findOne({ _id: "65e0b6a05d65c4b29ed92fbb" })
