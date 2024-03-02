9ff8fcf7b95e427f75e70fed2dc0f46f5b43129e
Diwash Karki <diwaskarki96@gmail.com>
2024-03-02 23:49:03 +0545

    some changes

--- modules/posts/post.model.js
+++ modules/posts/post.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
   postText: { type: String },
+  image: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "Members" },
 });
 const postModel = mongoose.model("Post", postSchema);
