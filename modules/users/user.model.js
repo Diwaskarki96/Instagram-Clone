a3ee99388790d9e1a3d969ac09876a6ff50d19d8
Diwash Karki <diwaskarki96@gmail.com>
2024-03-03 23:45:02 +0545

    minor changes

--- modules/users/user.model.js
+++ modules/users/user.model.js
@@ -4,6 +4,8 @@ const commonSchema = require("../../utils/commonSchema");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
+  profileimage: { type: String },
+  bio: { type: String },
   password: { type: String, required: true },
   posts: [
     {
