4efd498b14c17223b6ec8a9e724ca34910b133b6
Diwash Karki <diwaskarki96@gmail.com>
2024-03-22 17:33:24 +0545

    added timestamp in database model

--- modules/posts/post.model.js
+++ modules/posts/post.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  postText: { type: String },
-  image: { type: String },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Members" }],
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "Members" },
-});
+const postSchema = new mongoose.Schema(
+  {
+    postText: { type: String },
+    image: { type: String },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Members" }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "Members" },
+  },
+  { timestamps: true }
+);
 const postModel = mongoose.model("Post", postSchema);
 module.exports = postModel;
--- modules/users/user.model.js
+++ modules/users/user.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
-const commonSchema = require("../../utils/commonSchema");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
-  profileimage: { type: String },
-  bio: { type: String },
-  password: { type: String, required: true },
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post",
-    },
-  ],
-  // ...commonSchema.obj,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    profileimage: { type: String },
+    bio: { type: String },
+    password: { type: String, required: true },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("Members", userSchema);
 module.exports = userModel;
